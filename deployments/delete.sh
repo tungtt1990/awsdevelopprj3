590993dfbd31b5b54e990ed32dd8ef370e38c3d8
Tung TuanTran <tung.tuantran@coxautoinc.com>
2024-07-01 17:02:54 +0700

    update reverse proxy code

--- deployments/delete.sh
+++ deployments/delete.sh
@@ -1,9 +1,9 @@
 kubectl delete deployments udagram-api-feed
 kubectl delete deployments udagram-api-user
-kubectl delete deployments udagram-api-frontend
+kubectl delete deployments udagram-frontend
 kubectl delete deployments reverse-proxy
 
 kubectl delete services udagram-api-feed
 kubectl delete services udagram-api-user
-kubectl delete services udagram-api-frontend
+kubectl delete services udagram-frontend
 kubectl delete services reverse-proxy
